0bcc57d49bc6832361725aaecbb7571643897cff
Charlie Wolcott <wolcott@n2imaging.com>
2016-02-03 16:21:17 -0800

    Added:
    - Asteroid hit and destroy scripts
    --- Asteroids will be destroyed after 5 hits
    --- Asteroids play an explosion sound and VFX when destroyed
    --- Asteroids only destroy "bolts" that hit them. Not the player's ship or other asteroids
    --- Asteroids, when created, tumble randomly (need to make this a prefab then generate asteroids randomly in game area)
    
    - Player's shots (bolts) have sounds effects

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -4,11 +4,23 @@ using System.Collections;
 public class Mover : MonoBehaviour 
 {
     public float speed;
+    public float lifeTime;
 
     private Rigidbody rigidBody;
 
     void Start()
     {
         GetComponent<Rigidbody>().velocity = transform.forward * speed;
+
+        // Destroy the projectile in "life" seconds.
+        Invoke("Hit", lifeTime);
+    }
+
+    /**
+    * The method called when the DynamicObject component registers a collsion.
+    */
+    public void Hit()
+    {
+        Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/OnContactAsteroid.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnContactAsteroid : MonoBehaviour 
+{
+    public float hitsToDestroy;
+    public GameObject explosion;
+
+    void OnTriggerEnter(Collider other)
+    {
+        // If its a "bolt" that hit us
+        if (other.tag == "Bolt")
+        {
+            Destroy(other.gameObject);  // Destroy only bolts that hit us
+            
+            // hitting asteroids with bolts will destory them eventually.
+            // Player need to use their mining laser so asteroids are not destroyed!
+            hitsToDestroy--;
+        }
+
+        // For now, destroy the asteroid. For SpaceHunters though, we're going to generate crystals!
+        // After a certain amount of hits, then we destroy ourselves
+
+        if (0 == hitsToDestroy)
+        {
+            // Destroy ourselves
+            Destroy(gameObject);
+
+            // Show the explosion animation
+            if (explosion != null)
+            {
+                Instantiate(explosion, transform.position, transform.rotation);
+            }
+        }
+        else
+        {
+            // TODO: play a sound for hitting the asteroid
+
+            // TODO: show some animation for hitting the asteroid
+        }
+    }
+}
