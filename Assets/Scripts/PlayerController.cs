18bc4092f25f11a954a5816ac18f5cbc557f77b0
Charlie Wolcott <Charlie Wolcott>
2016-02-06 22:32:42 -0800

    Modified player's engine: - Flame stays oriented with ship when turning now - Using more siimple Quad to hold flame material instead of particle asset - Added code to make reverse speed less than forward speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,11 +9,13 @@ public class PlayerController : MonoBehaviour
     public float rotationSpeed;
 
     public GameObject shot;
+    public GameObject engines;
     public Transform shotSpawn;
     public float fireRate;
     private float nextFire;
 
     private Rigidbody rigidBody;
+    private float speedModifier;
 
 	// Use this for initialization
 	void Start () 
@@ -37,11 +39,24 @@ public class PlayerController : MonoBehaviour
         float yaw = Input.GetAxisRaw("Horizontal");
         float thrust = Input.GetAxisRaw("Vertical");
 
+        // Show engine thrust only when thrusting forward
+        //Debug.Log("thrust: " + thrust);
+        if (thrust > 0)
+        {
+            engines.SetActive(true);
+            speedModifier = 1.0f;
+        }
+        else
+        {
+            engines.SetActive(false);
+            speedModifier = 0.25f;
+        }
+
         // Apply turning as a rotation.
         transform.Rotate(0, yaw * Time.deltaTime * rotationSpeed, rigidBody.velocity.y);
 
         // Apply thrust as a force.
-        Vector3 force = transform.TransformDirection(0, 0, thrust * speed);
+        Vector3 force = transform.TransformDirection(0, 0, thrust * speed * speedModifier);
         rigidBody.AddForce(force);
 	}
 }
